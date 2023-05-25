b77a08b830555fc204f35fe12bac106b8276acac
Puntonatural <116592883+Puntonatural@users.noreply.github.com>
2023-05-25 10:47:41 -0500

    fix update producto

--- src/main/java/selly/cash/back/models/dao/IProductoDao.java
+++ src/main/java/selly/cash/back/models/dao/IProductoDao.java
@@ -3,5 +3,9 @@ package selly.cash.back.models.dao;
 import org.springframework.data.repository.CrudRepository;
 import selly.cash.back.models.entity.Producto;
 
+import java.util.Optional;
+
 public interface IProductoDao extends CrudRepository<Producto, Long> {
+
+    Optional<Producto> findById (Long id);
 }
